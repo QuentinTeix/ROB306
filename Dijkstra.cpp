6996d3a505d81f84eefceb8c6366d63cbbcafaea
QuentinTeix <qteix1602@gmail.com>
2021-09-28 11:59:40 +0200

    first commit

---
+++ Dijkstra.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include <fstream>
+#include<climits>
+using namespace std;
+
+const int DIMENSION = 20;
+
+int miniDist(int distance[], bool Tset[]) // finding minimum distance
+{
+    int minimum=INT_MAX,ind;
+              
+    for(int k=0;k<DIMENSION;k++) 
+    {
+        if(Tset[k]==false && distance[k]<=minimum)      
+        {
+            minimum=distance[k];
+            ind=k;
+        }
+    }
+    return ind;
+}
+
+void DijkstraAlgo(int graph[DIMENSION][DIMENSION],int src) // adjacency matrix 
+{
+    int distance[DIMENSION]; // // array to calculate the minimum distance for each node                             
+    bool Tset[DIMENSION];// boolean array to mark visited and unvisited for each node
+    
+     
+    for(int k = 0; k<DIMENSION; k++)
+    {
+        distance[k] = INT_MAX;
+        Tset[k] = false;    
+    }
+    
+    distance[src] = 0;   // Source vertex distance is set 0               
+    
+    for(int k = 0; k<DIMENSION; k++)                           
+    {
+        int m=miniDist(distance,Tset); 
+        Tset[m]=true;
+        for(int k = 0; k<DIMENSION; k++)                  
+        {
+            // updating the distance of neighbouring vertex
+            if(!Tset[k] && graph[m][k] && distance[m]!=INT_MAX && distance[m]+graph[m][k]<distance[k])
+                distance[k]=distance[m]+graph[m][k];
+        }
+    }
+    cout<<"Vertex\t\tDistance from source vertex"<<endl;
+    for(int k = 0; k<DIMENSION; k++)                      
+    { 
+        char str=65+k; 
+        cout<<str<<"\t\t\t"<<distance[k]<<endl;
+    }
+}
+
+int main()
+{
+    ifstream fs("C:/Users/qteix/OneDrive/Bureau/ENSTA/3A/ROB306-SEE/Projet/matrice.txt");
+
+    if (fs)
+    {
+        string line;
+        int i=0, j=0;
+
+        int tab[DIMENSION][DIMENSION];
+
+        while(getline(fs, line))
+        {
+            if (i<DIMENSION && j<DIMENSION) tab[i][j] = stoi(line);
+            else printf("Problème de taille");
+            i++;
+            if (i==DIMENSION)
+            {
+                i=0;
+                j++;
+            }
+        }
+        for (int i=0; i<DIMENSION; i++)
+        {
+            for (int j=0; j<DIMENSION; j++) std::cout << tab[i][j] << " ";
+            std::cout << std::endl;
+
+        }
+        DijkstraAlgo(tab,0);
+    }
+    else
+    {
+        printf("Problème de lecture");
+    }
+    
+    return 0;                           
+}
---
+++ generate.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <time.h>
+
+
+
+int main()
+{
+    srand(time(NULL));
+    std::ofstream fs("matrice.txt");
+
+    if(!fs)
+    {
+        std::cerr<<"Cannot open the output file." << std::endl;
+        return 1;
+    }
+    
+    printf("Enter the dimension : ");
+    int dimension;
+    scanf("%d", &dimension);
+
+    int l = (dimension-1)*dimension/2;
+    int tab[dimension][dimension];
+
+    for (int i=0; i<dimension; i++)
+    for (int j=i; j<dimension; j++)
+    {
+        if (i==j) tab[i][j]=0;
+        else
+        {
+            tab[i][j] = rand()%10;
+            tab[j][i] = tab[i][j];
+        }
+    }
+
+    for (int i = 0; i < dimension; i++)
+    {
+        for (int j = 0; j<dimension; j++)
+        {
+            fs << std::to_string(tab[i][j]) << "\n";
+        }
+    }
+    fs.close();
+    return 0;
+}
